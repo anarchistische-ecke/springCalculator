3f91c33c37ca157c55b1bc6b61c347a027b1dc22
freddycooper <almosttanonymous@gmail.com>
2024-06-04 23:10:34 +0300

    Assignment completed.

--- src/main/java/pro/sky/CalculatorApplication.java
+++ src/main/java/pro/sky/CalculatorApplication.java
@@ -1,4 +1,4 @@
-package pro.sky.calculator;
+package pro.sky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/pro/sky/controller/CalculatorController.java
@@ -0,0 +1,49 @@
+package pro.sky.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.service.CalculatorService;
+
+import static org.apache.coyote.http11.Constants.a;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Добро пожаловать в калькулятор!";
+    }
+
+    @GetMapping("/add")
+    public String sum(@RequestParam int a, @RequestParam int b) {
+        calculatorService.add(a,b);
+        return a + " + " + b + " = " + calculatorService.add(a, b);
+    }
+
+    @GetMapping("/substract")
+    public String substract(@RequestParam int a, @RequestParam int b) {
+        calculatorService.substract(a,b);
+        return a + " - " + b + " = " + calculatorService.substract(a, b);
+
+    }@GetMapping("/multiply")
+    public String multirply(@RequestParam int a, @RequestParam int b) {
+        calculatorService.multiply(a,b);
+        return a + " * " + b + " = " + calculatorService.multiply(a, b);
+    }
+
+    @GetMapping("/divide")
+    public String divide(@RequestParam int a, @RequestParam int b) {
+        calculatorService.add(a,b);
+        return a + " / " + b + " = " + calculatorService.divide(a, b);
+    }
+
+}
